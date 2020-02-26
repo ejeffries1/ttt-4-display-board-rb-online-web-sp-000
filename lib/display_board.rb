0e05eda414cca5785e78c67b7ea02568a174f33e
Evan Jeffries <ejeffries14@gmail.com>
2020-02-26 03:43:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = [" "," "," "," "," "," "," "," "," "]
+board = ["o"," "," "," "," "," "," "," "," "]
 def display_board(board)
   board
 end
